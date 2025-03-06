0a360e889209bbe38e071679f8121cf5eddc1ff6
unknown <daragopi69@gmail.com>
2025-03-06 22:46:37 +0530

    added button

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -4,6 +4,7 @@ function About() {
             About Page
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.</p>
             <h1>hi people helllo good to see</h1>
+            <button>click me</button>
         </div>
     );
 }
