27f1e008547ace4a99d6c7d84974f4a8220874e8
unknown <daragopi69@gmail.com>
2025-03-06 22:34:17 +0530

    added h1 tag by gopi

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,6 +3,7 @@ function About() {
         <div>
             About Page
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.</p>
+            <h1>hi people helllo good to see</h1>
         </div>
     );
 }
